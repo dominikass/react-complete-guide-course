3530e9b91d495cd833c626933657cad81ec53038
dominika <ds001@apriside.com>
2022-12-08 17:39:32 +0100

    adding expenses component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,11 @@
 import React from "react";
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 
 function App() {
-
-  const expenses = [
-    {
-      id: 1,
-      title: "Hairdryer",
-      price: 119,
-      date: new Date(2022, 10, 21)
-    },
-    {
-      id: 2,
-      title: "Toothbrush",
-      price: 75,
-      date: new Date(2022, 10, 15)
-    }
-  ];
-
-  return (
-    <div>
-      <ExpenseItem
-      title={expenses[0].title}
-      price={expenses[0].price}
-      date={expenses[0].date}
-    ></ExpenseItem>
-      <ExpenseItem
-      title={expenses[1].title}
-      price={expenses[1].price}
-      date={expenses[1].date}
-    ></ExpenseItem>
-    </div>
-  );
+return(
+  <Expenses/>
+)
 }
 
 export default App;
---
+++ src/components/Expenses.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css"
+
+
+function Expenses(){
+    const expenses = [
+        {
+          id: 1,
+          title: "Hairdryer",
+          price: 119,
+          date: new Date(2022, 10, 21)
+        },
+        {
+          id: 2,
+          title: "Toothbrush",
+          price: 75,
+          date: new Date(2022, 10, 15)
+        }
+      ];
+    
+      return (
+        <div className="expenses">
+          <ExpenseItem
+          title={expenses[0].title}
+          price={expenses[0].price}
+          date={expenses[0].date}
+        ></ExpenseItem>
+          <ExpenseItem
+          title={expenses[1].title}
+          price={expenses[1].price}
+          date={expenses[1].date}
+        ></ExpenseItem>
+        </div>
+      );
+}
+
+export default Expenses;
